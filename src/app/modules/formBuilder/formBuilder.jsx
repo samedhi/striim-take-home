66360a8361fe8e09c24da1a00b75eb8f321eacf0
samedhi <stephen@read-line.com>
2025-03-15 20:29:43 -0700

    Add placeholder renderer for all fields

--- src/app/modules/formBuilder/formBuilder.jsx
+++ src/app/modules/formBuilder/formBuilder.jsx
@@ -1,7 +1,19 @@
-'use client'
+'use client';
 
 import React, { useState } from 'react';
 
-export default function FormBuilder(props) {
-    return <div>FormBuilder component gets rendered here</div>;
+export default function FormBuilder({ formData }) {
+    const renderField = (field) => {
+        return <div>{ field.name }</div>;
+    };
+
+    return <form className="w-full max-w-lg space-y-6">
+             {formData.fields.map(field => (
+                 <div key={field.name} className="space-y-2">
+                   <label>{field.label}</label>
+                   {renderField(field)}
+                 </div>
+             ))}
+             <button type="submit">Submit</button>
+           </form>;
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import Image from "next/image";
 import FormBuilder from "./modules/formBuilder/formBuilder";
